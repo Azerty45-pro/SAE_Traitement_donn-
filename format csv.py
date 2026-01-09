9665470be111a5e0c39c3f9f045e0d5c854a6147
e2505535 <bilalfalana.pro@gmail.com>
2026-01-09 14:52:08 +0100

    csv

--- format csv.py
+++ format csv.py
@@ -12,3 +12,17 @@ with open('fichier.csv', mode='w', encoding='utf-8', newline='') as file:
     fichier = csv.DictWriter(file, fieldnames=champs)
     fichier.writeheader()
     fichier.writerows(etudiants)
+
+resultatetudiants = [
+    {"ID": 1, "Nom": "Schmitt", "Prenom": "Albert", "Resultat": "Refusé"},
+    {"ID": 2, "Nom": "Al-Hakim", "Prenom": "Yasmine", "Resultat": "Admis"},
+    {"ID": 3, "Nom": "Tran", "Prenom": "Sebastien",  "Resultat": "Admis"},
+    {"ID": 4, "Nom": "Meyer", "Prenom": "Claire",  "Resultat": "Admis"},
+    {"ID": 5, "Nom": "Kobayashi", "Prenom": "Kaito",  "Resultat": "Admis"}
+]
+
+with open('resultat.csv', mode='w', encoding='utf-8', newline='') as file:
+    champs = ["ID","Nom", "Prenom", "Resultat"]
+    resultat= csv.DictWriter(file, fieldnames=champs)
+    resultat.writeheader()
+    resultat.writerows(resultatetudiants)
